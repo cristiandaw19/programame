5c3720eb484162c86d5257cbe8b6bfac384d3716
cristiandaw19 <55553799+cristiandaw19@users.noreply.github.com>
2019-10-07 18:17:48 +0200

    Update p362.java

--- p362.java
+++ p362.java
@@ -1,18 +1,33 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class p362 {
-	
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+Scanner sc = new Scanner(System.in);
 		
 		int ncasos = sc.nextInt();
 		int dia;
 		int mes;
-
+		
 		for(int i=0; i<ncasos; i++){
-			
+			dia= sc.nextInt();
+			mes= sc.nextInt();
+		if(dia == 25 && mes == 12) {
+			System.out.println("SI");
+		}
+		
+		else {
+			System.out.println("NO");
+		}
+		
 			
 		}
-
+		
 	}
+
 }
