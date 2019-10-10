d7bd871f21673812257811795b908490f54cb443
Cristian Jaime <grancjrh@gmail.com>
2019-10-10 14:22:38 +0200

    programao

--- p362.java
+++ p362.java
@@ -15,7 +15,18 @@ Scanner sc = new Scanner(System.in);
 		int mes;
 		
 		for(int i=0; i<ncasos; i++){
+			dia= sc.nextInt();
+			mes= sc.nextInt();
+		if(dia == 25 && mes == 12) {
+			System.out.println("SI");
+		}
+		
+		else {
+			System.out.println("NO");
+		}
+		
 			
+		}
 		
 	}
 
